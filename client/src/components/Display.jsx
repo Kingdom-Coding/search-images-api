435a5d762110a3515c8e64f0169f75369799c385
Kingdom-Coding <jonathanv2283219@gmail.com>
2025-05-17 02:52:59 -0400

    added responsive masonry grid with columns, chose to take out image info

--- client/src/components/Display.jsx
+++ client/src/components/Display.jsx
@@ -5,19 +5,8 @@ export function Display({ results }) {
     <div className="display-wrapper">
       {results.map((result) => {
         return (
-          <div className="image-conatiner" key={result.id}>
-            <img src={result.webformatURL} alt={result.tags} />
-            <div className="image-status">
-              <img
-                id="user-image"
-                height="50px"
-                width="50px"
-                src={result.userImageURL}
-              />
-              <p>views: {result.views}</p>
-              <p>likes: {result.likes}</p>
-              <p>downloads: {result.downloads}</p>
-            </div>
+          <div className="image-container" key={result.id}>
+            <img id="img" src={result.webformatURL} alt={result.tags} />
           </div>
         );
       })}
