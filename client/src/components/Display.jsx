86fe8e0eaf748d3d96f5c2f71ced1c64c993cbe4
Kingdom-Coding <jonathanv2283219@gmail.com>
2025-05-17 10:39:24 -0400

    updated the Display css and added modal state, and image states for the modal to be displayed

--- client/src/components/Display.jsx
+++ client/src/components/Display.jsx
@@ -1,15 +1,35 @@
+import { Modal } from "./Modal";
+import { useState } from "react";
 import "./Display.css";
 
 export function Display({ results }) {
+  const [modal, setModal] = useState(false);
+  const [img, setImg] = useState(false);
+  function onClickImg(result) {
+    setModal(true);
+    setImg(result);
+    console.log(img);
+  }
+
   return (
-    <div className="display-wrapper">
-      {results.map((result) => {
-        return (
-          <div className="image-container" key={result.id}>
-            <img id="img" src={result.webformatURL} alt={result.tags} />
-          </div>
-        );
-      })}
+    <div className="display-container">
+      <div className="display-wrapper">
+        {results.map((result) => {
+          return (
+            <div className="image-container" key={result.id}>
+              <img
+                id="img"
+                src={result.webformatURL}
+                alt={result.tags}
+                onClick={() => {
+                  onClickImg(result);
+                }}
+              />
+            </div>
+          );
+        })}
+      </div>
+      {modal && <Modal img={img} setModal={setModal} />}
     </div>
   );
 }
